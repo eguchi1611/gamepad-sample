9fcc2d4d93c9930961b76ef16d054e20b1c0437f
Keita Ito <eguchi@eguchi.cc>
2024-10-22 00:57:04 +0900

    feat: canvasを表示

--- src/features/game/components/main-frame.tsx
+++ src/features/game/components/main-frame.tsx
@@ -1,3 +1,13 @@
+import Konva from "konva";
+import { Layer, Rect, Stage, Text } from "react-konva";
+
 export default function MainFrame() {
-  return <div className="h-full bg-red-500">a</div>;
+  return (
+    <Stage width={640} height={480}>
+      <Layer>
+        <Text text="Hello, World!" fontSize={64} />
+        <Rect x={20} y={20} width={50} height={50} fill={Konva.Util.getRandomColor()} />
+      </Layer>
+    </Stage>
+  );
 }
