16bf825a800fa4448def6ef543d3f25a80d73ffd
Keita Ito <eguchi@eguchi.cc>
2024-10-22 15:53:03 +0900

    refactor: input-providerの名称変更

--- src/app/(index)/_components/providers.tsx
+++ src/app/(index)/_components/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { InputProvider } from "@/features/gamepad/components/gamepad-provider";
+import { InputProvider } from "@/features/gamepad/components/input-provider";
 import type { PropsWithChildren } from "react";
 
 export function Providers({ children }: PropsWithChildren) {
