5e4aa0ed8bc65bd879642d7ccaded2c0ae38d1f2
Keita Ito <eguchi@eguchi.cc>
2024-10-26 03:23:26 +0900

    chore: 読み込み中をわかりやすく表示

--- src/features/game/components/sign-in-view.tsx
+++ src/features/game/components/sign-in-view.tsx
@@ -1,17 +1,20 @@
 import { auth, database } from "@/firebase";
 import { signInAnonymously } from "firebase/auth";
 import { ref, set } from "firebase/database";
-import { FormEvent, useCallback } from "react";
+import { FormEvent, useCallback, useState } from "react";
 
 export function SignInView() {
+  const [loading, setLoading] = useState(false);
   const handleSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     const { name } = Object.fromEntries(new FormData(e.currentTarget));
     const { user } = await signInAnonymously(auth);
     await set(ref(database, `users/${user.uid}`), {
       name,
       pos: { x: 0, y: 0 },
     });
+    setLoading(false);
   }, []);
 
   return (
@@ -23,7 +26,9 @@ export function SignInView() {
         </label>
       </div>
       <div>
-        <button type="submit">Sign In</button>
+        <button type="submit" disabled={loading}>
+          {loading ? "..." : "Sign In"}
+        </button>
       </div>
     </form>
   );
