47746c26d1b3ac970b246f30db8a213249b50f1e
Keita Ito <eguchi@eguchi.cc>
2024-10-26 02:41:18 +0900

    feat: 操作説明を追加

--- src/app/(index)/_components/view.tsx
+++ src/app/(index)/_components/view.tsx
@@ -8,14 +8,26 @@ const MainFrame = dynamic(() => import("@/features/game/components/main-frame"),
 
 export function View() {
   return (
-    <div className="grid h-full place-content-center">
+    <div className="grid h-full place-content-center space-y-2">
+      <table className="border">
+        <tbody>
+          <tr>
+            <th>キーボード</th>
+            <td>WASD: 移動, Shift: 高速, Control: 低速</td>
+          </tr>
+          <tr>
+            <th>ゲームパッド</th>
+            <td>左スティック: 移動, B: 高速, Y: 低速</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="border">
+        <MainFrame />
+      </div>
       <div>
         <button type="button" onClick={() => signOut(auth)}>
           Sign out
         </button>
-      </div>
-      <div className="border">
-        <MainFrame />
       </div>
     </div>
   );
